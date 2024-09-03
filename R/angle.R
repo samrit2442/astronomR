c343158f9c7f0f797a633cacf49289ca83ca45f1
SAMRIT PRAMANIK <samrit.2442@gmail.com>
2024-09-04 02:14:12 +0530

    degree to hours, minutes, seconds function updated

--- R/angle.R
+++ R/angle.R
@@ -64,5 +64,35 @@ dms_to_deg <- function(d, m, s, digit = 5) {
   return(df |> dplyr::pull(deg))
 }
 
+deg_to_hms <- function(deg, type = 'cat', digit = 5) {
+  DEG <- 1
+  HRS <- 1
+  MIN <- 60
+  SEC <- 3600
 
+  df <- tibble::tibble(deg) |>
+    dplyr::mutate(DEG = dplyr::if_else(deg < 0, deg + 360, deg),
+           HRS = floor(DEG/15),
+           MIN = floor((DEG/15 - HRS) * 60),
+           SEC = (DEG/15 - HRS - MIN/60) * 3600,
+           SEC = dplyr::case_when(SEC <= 0 ~ 0,
+                           SEC > 60 ~ 60,
+                           .default = SEC),
+           MIN = dplyr::case_when(SEC == 60 ~ MIN + 1,
+                           MIN == 60 ~ 0,
+                           .default = MIN),
+           HRS = dplyr::case_when(MIN == 60 ~ HRS + 1,
+                           .default = HRS),
+           HRS = HRS %% 24,
+           SEC = round(SEC, digits = 5),
+           output1 = paste0(stringr::str_c(HRS, MIN, SEC, sep = ":")),
+           output2 = paste0(HRS, "H", MIN, "M", SEC, "S"))
+
+  if(type == 'cat') {
+    return(cat(df$output2))
+  }
+  if(type == 'mat') {
+    return(df |> dplyr::select(HRS, MIN, SEC) |> as.matrix())
+  }
+}
 
