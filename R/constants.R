f7b944c6dd3f5006a0a37bd445a69bf453390e6c
SAMRIT PRAMANIK <samrit.2442@gmail.com>
2024-11-03 23:21:53 +0530

    Add Constant values

---
+++ R/constants.R
@@ -0,0 +1,14 @@
+# Create the object
+
+
+const_c0 <- 299792458
+const_mu0 <- 4e-7 * pi
+const_epsilon0 <- 8.854187817e-12
+
+
+
+
+
+save(const_c0, file = "data/const_c0.rda")
+save(const_mu0, file = "data/const_mu0.rda")
+save(const_epsilon0, file = "data/const_epsilon0.rda")
