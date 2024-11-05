0af6af0ec590d561c448044df3892f72992a831b
SAMRIT PRAMANIK <samrit.2442@gmail.com>
2024-11-05 21:30:20 +0530

    Add all physical constants

--- R/constants.R
+++ R/constants.R
@@ -1,13 +1,46 @@
 # Create the object
-
 const_c0 <- 299792458
 const_mu0 <- 4e-7 * pi
 const_epsilon0 <- 8.854187817e-12
-
-
+const_G <- 6.67408e-11
+const_h <- 6.626070040e-34
+const_e <- 1.6021766208e-19
+const_phi0 <- 2.067833831e-15
+const_G0 <- 7.7480917310e-5
+const_m_e <- 9.10938356e-31
+const_m_p <- 1.672621898e-27
+# const_m_e_
+const_alpha <- 7.2973525664e-3
+const_alpha_inverse <- 1/const_alpha
+const_R_infinity <- 10973731.568508
+const_L <- 6.022140857e23
+const_F <- 96485.33289
+const_R <- 8.3144598
+cosnt_k <- 1.38064852e-23
+const_sigma <- 5.670367e-8
+const_eV <- 1.6021766208e-19
+const_u <- 1.660539040e-27
 
 
 
 save(const_c0, file = "data/const_c0.rda")
 save(const_mu0, file = "data/const_mu0.rda")
 save(const_epsilon0, file = "data/const_epsilon0.rda")
+save(const_G, file = "data/const_G.rda")
+save(const_h, file = "data/const_h.rda")
+save(const_e, file = "data/const_e.rda")
+save(const_phi0, file = "data/const_phi0.rda")
+save(const_G0, file = "data/const_G0.rda")
+save(const_m_e, file = "data/const_m_e.rda")
+save(const_m_p, file = "data/const_m_p.rda")
+# const
+save(const_alpha, file = "data/const_alpha.rda")
+save(const_alpha_inverse, file = "data/const_alpha_inverse.rda")
+save(const_R_infinity, file = "data/const_R_infinity.rda")
+save(const_L, file = "data/const_L.rda")
+save(const_F, file = "data/const_F.rda")
+save(const_R, file = "data/const_R.rda")
+save(cosnt_k, file = "data/cosnt_k.rda")
+save(const_sigma, file = "data/const_sigma.rda")
+save(const_eV, file = "data/const_eV.rda")
+save(const_u, file = "data/const_u.rda")
