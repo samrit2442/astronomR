02c81501182b3848127479b231518430f2f1b0ad
SAMRIT PRAMANIK <samrit.2442@gmail.com>
2025-01-28 11:19:36 +0530

    Update Saha Equation documentation

--- R/06_thermo2.R
+++ R/06_thermo2.R
@@ -1,4 +1,3 @@
-library(pracma)
 zeta_3 <- 1.20205690316
 EI <- 13.6  # Hydrogen Binding (eV)
 Me <- 5.10999e5  # Mass of electron use the table
@@ -17,7 +16,21 @@ T <- function(z) {
 # Pre-factor for the Saha equation
 pre_fac_saha <- nb * (2 * pi / Me)^(3/2)
 
-photon_energy_density_fn_T <- function(T,unit="eV"){#T is in eV
+
+#' Photon Energy Density as a Function of Temperature
+#'
+#' This function calculates the photon energy density for a given temperature \(T\).
+#'
+#' @param T Numeric. Temperature in either eV or Kelvin.
+#' @param unit Character. The unit of temperature, either "eV" or "K". Defaults to "eV".
+#'
+#' @return Numeric. The photon energy density.
+#'
+#' @examples
+#' photon_energy_density_fn_T(1, "eV")
+#' photon_energy_density_fn_T(300, "K")
+#' @export
+photon_energy_density_fn_T <- function(T, unit = "eV"){#T is in eV
   if(unit=="eV"){
     a <- (pi^2/15)*T^4
   }
@@ -32,6 +45,20 @@ photon_energy_density_fn_T <- function(T,unit="eV"){#T is in eV
   return(a)
 }
 
+
+#' Photon Number Density as a Function of Temperature
+#'
+#' This function calculates the photon number density for a given temperature \(T\).
+#'
+#' @param T Numeric. Temperature in either eV or Kelvin.
+#' @param unit Character. The unit of temperature, either "eV" or "K". Defaults to "eV".
+#'
+#' @return Numeric. The photon number density.
+#'
+#' @examples
+#' photon_number_density_fn_T(1, "eV")
+#' photon_number_density_fn_T(300, "K")
+#' @export
 photon_number_density_fn_T <- function(T,unit="eV"){#T is in eV
   if(unit=="eV"){
     a <- (2*zeta_3/pi^2)*T^3
@@ -47,32 +74,69 @@ photon_number_density_fn_T <- function(T,unit="eV"){#T is in eV
   return(a)
 }
 
+
+#' Photon Energy Density as a Function of Redshift
+#'
+#' This function calculates the photon energy density for a given redshift \(z\).
+#'
+#' @param z Numeric. The redshift value.
+#'
+#' @return Numeric. The photon energy density.
+#'
+#' @examples
+#' photon_energy_density_fn_z(1300)
+#' @export
 photon_energy_density_fn_z <- function(z){
   return((pi^2/15)*T(z)^4)
 }
 
+
+#' Photon Number Density as a Function of Redshift
+#'
+#' This function calculates the photon number density for a given redshift \(z\).
+#'
+#' @param z Numeric. The redshift value.
+#'
+#' @return Numeric. The photon number density.
+#'
+#' @examples
+#' photon_number_density_fn_z(1300)
+#' @export
 photon_number_density_fn_z <- function(z){
   return((2*zeta_3/pi^2)*T(z)^3)
 }
 
+
+#' Ionization Fraction Using the Saha Equation
+#'
+#' This function calculates the ionization fraction \(X_e\) as a function of redshift \(z\) using the Saha equation.
+#'
+#' @param z Numeric. The redshift value.
+#'
+#' @return Numeric. The ionization fraction \(X_e\).
+#'
+#' @examples
+#' Saha_Xe(1300)
+#' @export
 Saha_Xe <- function(z) {
   f <- pre_fac_saha * T(z)^(3/2) * exp(EI / T(z))
   return((-1 + sqrt(1 + 4 * f)) / (2 * f))
 }
+
+
+#' Solution for Ionization Fraction \(X_e = 0.5\)
+#'
+#' This function solves the equation \(X_e - 0.5 = 0\) for the redshift \(z\).
+#'
+#' @param z Numeric. The redshift value.
+#'
+#' @return Numeric. The result of the equation \(X_e - 0.5\).
+#'
+#' @examples
+#' soln_saha(1350)
+#' @export
 soln_saha <- function(z) {
   return(Saha_Xe(z) - 0.5)
-}
-recombination_temp_saha <- function(res_type="Z"){
-  z_rec <- fzero(soln_saha, c(1300, 1400))$x
-  if(res_type=="Z"){
-    result <- z_rec
-    print("Red-shift value")
-  }
-  else{
-      result <- T(z_rec)/8.6173e-5
-      print("Temp in Kelvin")
-    }
-  return(result)
 }
 
 
